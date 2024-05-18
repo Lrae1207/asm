aae77bb5d6aab52d1a4cd3a441134a6228cec3d6
Lrae <lraeprogramming@gmail.com>
2024-05-17 19:50:45 -0600

    Update dbg.sh

--- dbg.sh
+++ dbg.sh
@@ -12,7 +12,7 @@ fi
 if [ "$?" -eq "0" ]; then
     echo "Compiled and linked successfully"
     echo "Debugging $1"
-    gdb "$1" -ex "set disassembly-flavor intel" -ex "break _start"
+    gdb "$1" -ex "set disassembly-flavor intel" -ex "break _start" -ex "disassemble _start"
 else
     echo "Link failed"
     exit 1
